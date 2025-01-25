404e6feb0577c9b560e86377e788c913a270155a
unknown <romane.nicole27@gmail.com>
2025-01-26 00:17:29 +0100

    first commit

---
+++ L'eau/src/App.tsx
@@ -0,0 +1,28 @@
+
+import './App.css'
+import videobg from './assets/Ocean 4K Vidéo.mp4'
+
+function App() {
+  return (
+    <div> 
+      <div class='bg'>
+        <video src={videobg} autoPlay="autoplay" loop="infinite"></video>
+      
+          <div id='titre'> 
+          <h1>Tapery Oil</h1>
+          <h2>Depuis 1991</h2>
+
+          </div>
+         
+
+    </div>
+  </div>
+
+
+    
+
+
+  )
+}
+
+export default App
